bfc8fb01a6c3329b77a9e482661161c227ac035b
jb12-art <jbqw1@msn.com>
2025-05-28 20:04:03 +0300

    dashboard mobile/desktop view, added 3 mobile icons and 3 desktop icons to icon folder from figma

---
+++ src/script/dashboard-student.js
@@ -0,0 +1,62 @@
+// mobile view
+
+// h1 heading
+const Heading = document.createElement('h1');
+Heading.innerHTML = 'Dashboard';
+
+const dashboard = document.getElementById('heading');
+dashboard.appendChild(Heading);
+
+// student initials img
+function studentInitialsImg() {
+  const imgDiv = document.getElementById('student-initials-mobile');
+  const imgIcon = document.createElement('img');
+  imgIcon.src = '/public/assets/img/Student initials mobile.png';
+  imgIcon.alt = 'student initials icon';
+  imgDiv.appendChild(imgIcon);
+}
+studentInitialsImg();
+
+// student name
+const studentName = document.createElement('h2');
+studentName.innerHTML = 'Joe Bloggs';
+
+const studentContainer = document.getElementById('student-name');
+studentContainer.appendChild(studentName);
+
+// student title
+const studentTitle = document.createElement('p');
+studentTitle.innerHTML = 'Student at Edugate school';
+
+const studentT = document.getElementById('student-title');
+studentT.appendChild(studentTitle);
+
+// profile img
+function profileIcon() {
+  const imgDiv = document.getElementById('profile-icon');
+  const imgIcon = document.createElement('img');
+  imgIcon.src = '/public/assets/icons/profile.png';
+  imgIcon.alt = 'student icon';
+  imgDiv.appendChild(imgIcon);
+}
+profileIcon();
+
+// my results icon
+function myResultsIcon() {
+  const imgDiv = document.getElementById('my-results-icon');
+  const imgIcon = document.createElement('img');
+  imgIcon.src = '/public/assets/icons/my-results.png';
+  imgIcon.alt = 'profile icon';
+  imgDiv.appendChild(imgIcon);
+}
+myResultsIcon();
+
+// logout icon
+function logoutIcon() {
+  const imgDiv = document.getElementById('logout-icon');
+  const imgIcon = document.createElement('img');
+  imgIcon.src = '/public/assets/icons/logout.png';
+  imgIcon.alt = 'logout icon';
+  imgDiv.appendChild(imgIcon);
+}
+logoutIcon();
