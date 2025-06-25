b749cb175e19ecdcf0d89accd9450282f41b4d11
jb12-art <jbqw1@msn.com>
2025-06-25 16:06:41 +0300

    new .json file

--- src/script/sign-up.js
+++ src/script/sign-up.js
@@ -4,34 +4,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // add event listener to close button
   closeBtn.addEventListener('click', () => {
-    document.getElementById('success-popup').classList.add('hidden');
+    document.getElementById('fail-popup').classList.add('hidden');
   });
 
   form.addEventListener('submit', async (event) => {
     event.preventDefault(); // prevent default form submission
 
     const emailInput = document.getElementById('email').value.trim();
+    const firstNameInput = document.getElementById('first-name').value.trim();
+    const surnameInput = document.getElementById('surname').value.trim();
     const passwordInput = document.getElementById('password').value.trim();
+    const confirmPasswordInput = document
+      .getElementById('confirm-password')
+      .value.trim();
     const errorMessage = document.getElementById('error-message');
 
     try {
-      const response = await fetch('/src/script/api/login-user.json');
+      const response = await fetch('/src/script/api/signup-user.json');
       const user = await response.json();
 
       const matchedUser = user.find(
-        (user) => user.email === emailInput && user.password === passwordInput
+        (user) =>
+          user.email === emailInput &&
+          user.firstName === firstNameInput &&
+          user.surname === surnameInput &&
+          user.password === passwordInput &&
+          user.confirmPassword === confirmPasswordInput
       );
 
       if (matchedUser) {
         errorMessage.classList.add('hidden'); // hide previous error
         showPopup();
         setTimeout(() => {
-          window.location.href = '/src/pages/dashboard-student.html';
+          window.location.href = '/src/pages/log-in.html';
         }, 3000); // Redirect after 3 seconds
-      } else {
-        const errorMessage = document.getElementById('error-message');
-        errorMessage.textContent = 'Invalid email or password';
-        errorMessage.classList.remove('hidden');
       }
     } catch (error) {
       console.error('Error fetching user data:', error);
@@ -43,11 +49,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function showPopup() {
-  document.getElementById('success-popup').classList.remove('hidden');
+  document.getElementById('fail-popup').classList.remove('hidden');
 }
 
 function closePopup() {
-  document.getElementById('success-popup').classList.add('hidden');
+  document.getElementById('fail-popup').classList.add('hidden');
 }
 
 window.closePopup = closePopup;
